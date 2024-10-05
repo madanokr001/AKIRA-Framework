4dfd070402b33bdf35816f40bfb22ffe178b6745
건우Sec <madanokr002@gmail.com>
2024-10-05 15:55:27 +0900

    Update AKIRA.py

--- PROJECT/AKIRA.py
+++ PROJECT/AKIRA.py
@@ -42,7 +42,7 @@ def logo():
 -- BACKDOOR CREATER --
 -- github : github.com/madanokr001 --
 -- Version 1.0 --
--- www.revshells.com -- 
+-- use www.revshells.com -- 
           
 [1] NMAP
 [2] MSFVENOM
